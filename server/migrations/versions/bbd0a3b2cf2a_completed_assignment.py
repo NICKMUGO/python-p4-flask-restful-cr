06f6c564346ffa0b69a8da571cbc9c0705713049
Nick Mugo <nicknjuru13@gmail.com>
2023-12-02 16:44:10 +0300

    Completed assignment

---
+++ server/migrations/versions/bbd0a3b2cf2a_completed_assignment.py
@@ -0,0 +1,24 @@
+"""Completed assignment
+
+Revision ID: bbd0a3b2cf2a
+Revises: be3f72f81f03
+Create Date: 2023-12-02 16:36:54.781954
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bbd0a3b2cf2a'
+down_revision = 'be3f72f81f03'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
